62c4949203b6499f7af737a8dd8161a724f01541
Jason Burgess <jason.nzd@gmail.com>
2023-01-09 21:26:19 +1300

    integrated azure storage for product images

---
+++ azure-storage.js
@@ -0,0 +1,28 @@
+import { BlobServiceClient } from '@azure/storage-blob';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// Create the BlobServiceClient object with connection string from .env
+const AZURE_STORAGE_CONSTRING = process.env.AZURE_STORAGE_CONSTRING;
+if (!AZURE_STORAGE_CONSTRING) {
+  throw Error('Azure Storage Connection string not found');
+}
+const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONSTRING);
+
+// Create ContainerClient for container 'countdownimages'
+const containerClient = blobServiceClient.getContainerClient('countdownimages');
+
+export default async function uploadImageToAzureStorage(id, imageUrl) {
+  // Use id as the filename
+  const blobFilename = id + '.jpg';
+  const blobClient = containerClient.getBlobClient(blobFilename);
+  // console.log('Attempting upload for id=' + id + ' \t url: ' + imageUrl.slice(29));
+
+  // If image doesn't already exist on azure, copy over
+  // if (!blobClient.exists()) {
+  const uploadBlobResponse = await blobClient.syncCopyFromURL(imageUrl);
+  if (uploadBlobResponse.copyStatus === 'success') {
+    // console.log('New image ' + blobFilename + ' uploaded successfully');
+  } else console.log(uploadBlobResponse);
+  // } else console.log(blobFilename + ' already exists');
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import playwright from 'playwright';
 import * as cheerio from 'cheerio';
 import _ from 'lodash';
 import { CosmosClient } from '@azure/cosmos';
+import uploadImageToAzureStorage from './azure-storage.js';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -37,6 +38,7 @@ const containerName = 'products';
 const partitionKey = ['/name'];
 
 // Create Cosmos client using connection string stored in .env
+console.log(`--- Connecting to CosmosDB..`);
 const cosmosClient = new CosmosClient(process.env.COSMOS_CONSTRING);
 
 // Connect to price database
@@ -47,27 +49,38 @@ const { container } = await database.containers.createIfNotExists({
   id: containerName,
   partitionKey: { paths: partitionKey },
 });
-console.log(`CosmosDB connected`);
 
 // Create a playwright browser using webkit
+console.log(`--- Launching Headless Browser..`);
 const browser = await playwright.webkit.launch({
   headless: true,
 });
 const page = await browser.newPage();
 
 // Open url
+console.log('--- Loading Webpage..');
 await page.goto('https://www.countdown.co.nz/shop/browse/fridge-deli');
-
-// Array of products to be pushed into
-const products = [];
 
 // Load all dynamic html into Cheerio for easy DOM selection
 const html = await page.evaluate(() => document.body.innerHTML);
 const $ = cheerio.load(html);
-console.log('Webpage loaded and ready to be scraped');
+
+// Print formatted table header to console
+console.log(
+  '\n  ID '.padEnd(6) +
+    ' | ' +
+    'Product Name'.padEnd(50) +
+    ' | ' +
+    'Price' +
+    ' |' +
+    '\n--------------------------------------------------------------------'
+);
 
 // Loop through each product card and build a product object with the desired data fields
 $('a.product-entry').each((index, productCard) => {
+  // Init empty product object
+  let product = {};
+
   // Extract ID from h3 tag and remove non-numbers
   product.id = $(productCard).find('h3').first().attr('id').replace(/\D/g, '');
 
@@ -86,19 +99,32 @@ $('a.product-entry').each((index, productCard) => {
   // Product size may be blank
   product.size = $(productCard).find('div.product-meta p span.size').text().trim();
 
-  product.image = $(productCard)
-    .find('a.product-entry div.productImage-container figure picture img')
-    .attr('src');
-
   product.lastUpdated = new Date().toDateString();
 
-  product.sourceSite = 'Countdown';
+  product.sourceSite = 'countdown.co.nz';
 
-  // Log completed product object
-  console.log(product);
+  // Log completed product object into a formatted table row
+  console.log(
+    product.id.padEnd(6) +
+      ' | ' +
+      product.name
+        .slice(0, 50)
+        .concat(' ' + product.size)
+        .padEnd(50) +
+      ' | ' +
+      product.price.toString().padStart(2).padEnd(4) +
+      ' |'
+  );
 
-  // Insert or update item into cosmosdb
+  // Insert or update item into azure cosmosdb
   container.items.upsert(product);
+
+  // Get image url, request hi-res 900px version, and then upload image to azure storage
+  const originalImageUrl = $(productCard)
+    .find('a.product-entry div.productImage-container figure picture img')
+    .attr('src');
+  const hiresImageUrl = originalImageUrl.replace('&w=200&h=200', '&w=900&h=900');
+  uploadImageToAzureStorage(product.id, hiresImageUrl);
 });
 
 // Close playwright headless browser
