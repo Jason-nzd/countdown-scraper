21fbf91945406af5f076da04c07e5674a2e9d95e
Jason Burgess <jason.nzd@gmail.com>
2023-04-12 19:07:44 +1200

    added more size overrides

--- src/data-overrides.ts
+++ src/data-overrides.ts
@@ -1,7 +1,10 @@
 export const overriddenProducts = [
-  {
-    name: 'Snickers Fun Size Chocolate Bars Share Bag',
-    id: '206889',
-    size: '180g',
-  },
+  // Snickers Fun Size Chocolate Bars Share Bag
+  { id: '206889', size: '180g' },
+
+  // Posh Poppas Camembert
+  { id: '196996', size: '300g' },
+
+  // Cool Gardener Falafel
+  { id: '137967', size: '420g' },
 ];
