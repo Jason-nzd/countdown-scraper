e9985a7120670e72dbede4bb117b764a950c1e6b
Jason Burgess <jason.nzd@gmail.com>
2023-04-14 15:29:08 +1200

    added more size overrides

--- src/data-overrides.ts
+++ src/data-overrides.ts
@@ -3,4 +3,7 @@ export const overriddenProducts = [
   { id: '196996', size: '300g' },
   { id: '137967', size: '420g' },
   { id: '125856', size: '450g' },
+  { id: '189268', size: '1.13kg' },
+  { id: '189150', size: '1.2kg' },
+  { id: '190454', size: '2.1kg' },
 ];
