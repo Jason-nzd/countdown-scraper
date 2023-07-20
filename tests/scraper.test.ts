03e5649696533de882d6d67e68c45b281a6c3f4b
Jason Burgess <jason.nzd@gmail.com>
2023-07-20 12:47:00 +1200

    updated tests

--- tests/scraper.test.ts
+++ tests/scraper.test.ts
@@ -1,9 +1,9 @@
 import 'jest';
-import { deriveCategoriesFromUrl } from '../src/index';
+import { parseAndCategoriseURL } from '../src/index';
 
 describe('Scraper', () => {
-  it('deriveCategoriesFromUrl_correct_categories', async () => {
-    const result = deriveCategoriesFromUrl(
+  it('parseAndCategoriseURL_correct_categories', async () => {
+    const result = parseAndCategoriseURL(
       'https://www.countdown.co.nz/shop/browse/pantry/snacks-sweets/corn-chips-salsa'
     );
     expect(result).toBe(['snacks-sweets', 'corn-chips-salsa']);
