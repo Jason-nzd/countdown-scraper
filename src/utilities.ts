90511ee29a1fa579f005c3e9a8384181f109f78d
Jason B <jason.nzd@gmail.com>
2024-10-04 17:02:44 +1300

    added toTitleCase function

--- src/utilities.ts
+++ src/utilities.ts
@@ -334,3 +334,10 @@ export const validCategories: string[] = [
   'dog-food',
   'dog-treats',
 ];
+
+// Convert a string to title case
+export function toTitleCase(str: string) {
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
