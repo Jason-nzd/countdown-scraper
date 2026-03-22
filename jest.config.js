44daa836069bb9a269086d8b9ce74ac5e2e22625
Jason B <jason.nzd@gmail.com>
2026-03-22 18:15:53 +1300

    switching testing to vitest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.(ts|tsx)?$': ['ts-jest', { diagnostics: { ignoreCodes: ['TS151001'] } }],
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
-};
